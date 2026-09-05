71aa22705ea1972b4f789af803224248c3f1af1b
AFRAJ AHAMATH <23da2-0545@sltc.ac.lk>
2026-09-05 14:17:12 +0530

    TreatmentRecord.java

---
+++ TreatmentRecord.java
@@ -0,0 +1,41 @@
+/**
+ * Represents a completed treatment event, stored on the Treatment History stack.
+ */
+public class TreatmentRecord {
+
+    private int patientId;
+    private String patientName;
+    private String treatmentDetails;
+    private String completionTime;
+
+    public TreatmentRecord(int patientId, String patientName, String treatmentDetails, String completionTime) {
+        this.patientId = patientId;
+        this.patientName = patientName;
+        this.treatmentDetails = treatmentDetails;
+        this.completionTime = completionTime;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public String getTreatmentDetails() {
+        return treatmentDetails;
+    }
+
+    public String getCompletionTime() {
+        return completionTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient ID: " + patientId +
+                " | Name: " + patientName +
+                " | Treatment: " + treatmentDetails +
+                " | Completed: " + completionTime;
+    }
+}
